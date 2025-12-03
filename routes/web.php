81b050cd8d23addbf2df2a3cf1ef25b3d15abe7b
Hezkiel09 <kielsiahaan09@gmail.com>
2025-12-04 01:32:39 +0700

    Add Route buat ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('web.dashboard');
+});
 
+
+// Profile route (Breeze)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
